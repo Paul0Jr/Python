b1d2c016f35a13cb271c30044a1b57cd13ec76c2
paul0jr <pjunior954@gmail.com>
2023-09-04 14:05:44 -0300

    Ex 83

--- exercícios/ex83.py
+++ exercícios/ex83.py
@@ -1,3 +1,21 @@
 '''Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. 
 Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.'''
 
+parent=[]
+frase=str(input("Digite uma expresão utilizando parênteses: "))
+
+for simb in frase:
+    if simb=="(":
+        parent.append("(")
+    elif simb==")":
+        if len(parent)>0:
+            parent.pop()
+        else:
+            parent.append(")")
+
+            break
+
+if len(parent) == 0:
+    print("Expressão válida!")
+else:
+    print("Expressão inválida!")
--- exercícios/ex84.py
+++ exercícios/ex84.py
@@ -3,3 +3,21 @@
  B) Uma listagem com as pessoas mais pesadas;
  C) Uma listagem com as pessoas mais leves.'''
 
+cadastro=[]
+dados=[]
+cont=0
+while True:
+    dados.append(str(input("Digite seu nome: ")))
+    dados.append(int(input("Digite seu peso: ")))
+    cadastro.append(dados[:])
+    dados.clear()
+    cont+=1
+    esc=str(input("Deseja continuar? S/N")).upper()
+    if esc in "SN":
+        if esc=='N':
+            break
+    else:
+        print("Responda apenas com S para Sim e N para Não!")
+        esc=str(input("Deseja continuar? S/N")).upper()
+
+print(cadastro)
